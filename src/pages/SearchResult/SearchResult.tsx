64ed9588d8e67b3be3b3cb61249b8b3b4f741cdf
emilyhanyf <emilyhyf@stanford.edu>
2024-09-02 17:32:18 -0700

    fixed scrolling section

--- src/pages/SearchResult/SearchResult.tsx
+++ src/pages/SearchResult/SearchResult.tsx
@@ -6,6 +6,7 @@ import { useLocation } from "react-router-dom";
 import SearchForm from "../../components/SearchForm/SearchForm";
 import ScrollableSection from "../../components/ScrollableSection/ScrollableSection";
 import SourcePage from "../../components/SourcePage/SourcePage";
+import Preview from "../../assets/Preview.png";
 
 const SearchResult: React.FC = () => {
   const location = useLocation();
@@ -23,6 +24,16 @@ const SearchResult: React.FC = () => {
       description:
         "240809 RAG_백서.pdf (페이지 13) …PhnyX Lab의 RAG 솔루션은 뛰어난 정확도, 확장성, 견고한 시스템, 그리고 다른 AI 어플리케이션과의 뛰어난 호환성을 통해 정보 검색의 새로운 패러다임을 제시합니다. 이 솔루션의 특장점은 아래와 같이 세분화가 가능합니다. 3.1.1 모듈러 RAG RAG 기술은 다양한 방법론과 기술로 구현될 수 있으며… 240812 Business Model.ppt (페이지 2) …목표: RAG-as-a-Service를 시작으로, PhnyX Lab은 AI를 기업 업무환경에 도입할 것입니다. 주요 내용: RAG-as-a-Service: 핵심 솔루션으로 애플리케이션 및 SDK를 포함하여 제공됩니다… 240720 RAG_백서.pdf (페이지 15) …PhnyX Lab 의 RAG 솔루션은 모듈러 RAG (Modular RAG) 구조를 채택함으로써 고객들에게 기술적 차별화 및 유연성을 제공할 예정입니다. 이러한 구조를 따른 프레임워크를 구축함으로써, 유스케이스에 따라 완벽히 최적화된 솔루션을 제공할 수 있도록 합니다. 3.2.1 기존 RAG 프레임워크 현존하는 대부분의 RAG 시스템은",
     },
+    {
+        title: "참고 자료",
+        description:
+          "240809 RAG_백서.pdf (페이지 13) …PhnyX Lab의 RAG 솔루션은 뛰어난 정확도, 확장성, 견고한 시스템, 그리고 다른 AI 어플리케이션과의 뛰어난 호환성을 통해 정보 검색의 새로운 패러다임을 제시합니다. 이 솔루션의 특장점은 아래와 같이 세분화가 가능합니다. 3.1.1 모듈러 RAG RAG 기술은 다양한 방법론과 기술로 구현될 수 있으며… 240812 Business Model.ppt (페이지 2) …목표: RAG-as-a-Service를 시작으로, PhnyX Lab은 AI를 기업 업무환경에 도입할 것입니다. 주요 내용: RAG-as-a-Service: 핵심 솔루션으로 애플리케이션 및 SDK를 포함하여 제공됩니다… 240720 RAG_백서.pdf (페이지 15) …PhnyX Lab 의 RAG 솔루션은 모듈러 RAG (Modular RAG) 구조를 채택함으로써 고객들에게 기술적 차별화 및 유연성을 제공할 예정입니다. 이러한 구조를 따른 프레임워크를 구축함으로써, 유스케이스에 따라 완벽히 최적화된 솔루션을 제공할 수 있도록 합니다. 3.2.1 기존 RAG 프레임워크 현존하는 대부분의 RAG 시스템은",
+      },
+      {
+        title: "참고 자료",
+        description:
+          "240809 RAG_백서.pdf (페이지 13) …PhnyX Lab의 RAG 솔루션은 뛰어난 정확도, 확장성, 견고한 시스템, 그리고 다른 AI 어플리케이션과의 뛰어난 호환성을 통해 정보 검색의 새로운 패러다임을 제시합니다. 이 솔루션의 특장점은 아래와 같이 세분화가 가능합니다. 3.1.1 모듈러 RAG RAG 기술은 다양한 방법론과 기술로 구현될 수 있으며… 240812 Business Model.ppt (페이지 2) …목표: RAG-as-a-Service를 시작으로, PhnyX Lab은 AI를 기업 업무환경에 도입할 것입니다. 주요 내용: RAG-as-a-Service: 핵심 솔루션으로 애플리케이션 및 SDK를 포함하여 제공됩니다… 240720 RAG_백서.pdf (페이지 15) …PhnyX Lab 의 RAG 솔루션은 모듈러 RAG (Modular RAG) 구조를 채택함으로써 고객들에게 기술적 차별화 및 유연성을 제공할 예정입니다. 이러한 구조를 따른 프레임워크를 구축함으로써, 유스케이스에 따라 완벽히 최적화된 솔루션을 제공할 수 있도록 합니다. 3.2.1 기존 RAG 프레임워크 현존하는 대부분의 RAG 시스템은",
+      },
   ];
 
   const handleSearch = async (query: string) => {
@@ -65,9 +76,7 @@ const SearchResult: React.FC = () => {
       </div>
 
       <div className="main-results">
-        <div>
-          <ScrollableSection items={searchResults} />
-        </div>
+        <ScrollableSection items={searchResults} />
         <SourcePage />
       </div>
 
